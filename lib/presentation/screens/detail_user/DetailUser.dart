b5c71f3ef06eb743bd4f42a93680c3f7971b9268
CKenny27 <106782235+CKenny27@users.noreply.github.com>
2023-03-30 11:23:21 -0500

    Actualizar detalles

--- lib/presentation/screens/detail_user/DetailUser.dart
+++ lib/presentation/screens/detail_user/DetailUser.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/presentation/screens/detail_user/widgets/info.dart';
+import 'package:flutter_application_1/presentation/screens/detail_user/widgets/list_post.dart';
 
 class DetailUserScreen extends StatelessWidget {
   static String routeName = 'detail_user';
@@ -7,67 +9,41 @@ class DetailUserScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Detalle Usuario'),
-          backgroundColor: const Color.fromRGBO(14, 126, 57, 0.997),
-          titleTextStyle: const TextStyle(
-              fontSize: 36.0, fontStyle: FontStyle.italic, color: Colors.white),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.delete_outlined,
-                color: Colors.white,
-              ),
-            )
+      appBar: AppBar(
+        title: const Text('Detalle Usuario'),
+        backgroundColor: const Color.fromRGBO(14, 126, 57, 0.997),
+        titleTextStyle: const TextStyle(
+            fontSize: 36.0, fontStyle: FontStyle.italic, color: Colors.white),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.delete_outlined,
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            InfoWidget(),
+            const SizedBox(
+              height: 10.0,
+            ),
+            ListPostWidget()
           ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Text(
-                'Informacion del usuario',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Card(
-                elevation: 20,
-                child: Column(
-                  children: [
-                    Column(
-                      children: [
-                        _item('Nombre', Icons.person),
-                        _item('correo@correo.com', Icons.email),
-                        _item(
-                            'Masculino', Icons.supervised_user_circle_rounded),
-                        _item('Activo', Icons.circle),
-                      ],
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-        ));
-  }
-
-  Widget _item(String text, IconData icon) {
-    return Column(
-      children: [
-        ListTile(
-          leading: Icon(
-            icon,
-            color: Colors.grey,
-            size: 40.0,
-          ),
-          trailing: Text(text),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: const Color.fromRGBO(14, 126, 57, 0.997),
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
         ),
-        const Divider()
-      ],
+      ),
     );
   }
 }
---
+++ lib/presentation/screens/detail_user/widgets/info.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class InfoWidget extends StatelessWidget {
+  const InfoWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const Text(
+          'Informacion del usuario',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Card(
+          elevation: 20,
+          child: Column(
+            children: [
+              Column(
+                children: [
+                  _item('Nombre', Icons.person),
+                  _item('correo@correo.com', Icons.email),
+                  _item('Masculino', Icons.supervised_user_circle_rounded),
+                  _item('Activo', Icons.circle),
+                ],
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _item(String text, IconData icon) {
+    return Column(
+      children: [
+        ListTile(
+          leading: Icon(
+            icon,
+            color: Colors.grey,
+            size: 40.0,
+          ),
+          trailing: Text(text),
+        ),
+        const Divider()
+      ],
+    );
+  }
+}
